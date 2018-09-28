49321bca37c7dd7e502307ecece3192890d70d34
Pavel Ravvich <triodjangopiter@yandex.ru>
2018-09-28 03:14:32 +0300

    Complete quiz layout

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import classes from './ActiveQuiz.css'
+import AnswersList from './AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
         <strong>2.</strong>&nbsp;
-        Lorem ipsum dolor sit amet
+        Как дела?
       </span>
 
       <small>4 from 12</small>
     </p>
 
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
+    <AnswersList
+      answers={props.answers}
+    />
   </div>
 );
 
