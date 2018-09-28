e2ca247e5c5c7c9cd56f4a8fd30788d534ac1304
Pavel Ravvich <triodjangopiter@yandex.ru>
2018-09-29 00:41:30 +0300

    Add question styles change logic

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,6 +6,7 @@ class Quiz extends Component {
 
   state = {
     activeQuestion: 0,
+    answerState: null,
     quiz: [
       {
         id: 1,
@@ -33,13 +34,49 @@ class Quiz extends Component {
   };
 
   onAnswerClickHandler = answerId => {
-    console.log(answerId);
-    this.setState((prevState) => {
-      return {
-        activeQuestion: prevState.activeQuestion + 1
-      };
-    })
+
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0];
+      if (this.state.answerState[key] === 'success') {
+        return;
+      }
+    }
+
+    const question = this.state.quiz[this.state.activeQuestion];
+
+    if (question.rightAnswerId === answerId) {
+
+      this.setState({
+        answerState: {[answerId]:'success'}
+      });
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          console.log('finish')
+        } else {
+          this.setState((prevState) => {
+            return {
+              activeQuestion: prevState.activeQuestion + 1,
+              answerState: null
+            }
+          })
+        }
+
+        window.clearTimeout(timeout);
+      }, 2000);
+
+    } else {
+      this.setState({
+        answerState: {[answerId]:'error'}
+      });
+    }
+
+
   };
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length;
+  }
 
   render() {
     return (
@@ -52,6 +89,7 @@ class Quiz extends Component {
             onAnswerClick={this.onAnswerClickHandler}
             quizLength={this.state.quiz.length}
             answerNumber={this.state.activeQuestion + 1}
+            state={this.state.answerState}
           />
         </div>
       </div>
