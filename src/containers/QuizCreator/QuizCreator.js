2bc1d8f019d25cdab5767e6374b7859362aa6160
Pavel Ravvich <triodjangopiter@yandex.ru>
2018-10-08 08:04:50 +0300

    Add QuizCreator component

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,101 @@
 import React, {Component} from 'react';
+import classes from './QuizCreator.css'
+import Button from "../../components/UI/Button/Button";
+import {createControl} from "../../form/formFramework";
+import Input from "../../components/UI/Input/Input";
+
+function createOptionControl(number) {
+  return createControl({
+    label: `Вариант ${number}`,
+    errorMessage: 'Значение не может быть пустым',
+    id: number
+  }, {required: true})
+}
+
+function createFormControls() {
+  return {
+    question: createControl({
+      label: 'Введите вопрос',
+      errorMessage: 'Вопрос не может быть пустым'
+    }, {required: true}),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4)
+  }
+}
 
 class QuizCreator extends Component {
+
+  state = {
+    quiz: [],
+    formControls: createFormControls()
+  };
+
+  obSubmitHandler = event => {
+    event.preventDefault();
+  };
+
+  addQuestionHandler = () => {
+
+  };
+
+  createQuizHandler = () => {
+
+  };
+
+  changeHandler = (event, controlName) => {
+
+  };
+
+  renderControls() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <React.Fragment key={index}>
+          <Input
+            label={control.label}
+            value={control.value}
+            valid={control.valid}
+            touched={control.touched}
+            errorMessage={control.errorMessage}
+            shouldValidate={!!control.validation}
+            onChange={event => this.changeHandler(event, controlName)}
+          />
+          {index === 0 ? <hr/> : null}
+        </React.Fragment>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+
+          <form onSubmit={this.obSubmitHandler}>
+
+            {this.renderControls()}
+
+            <select></select>
+
+            <Button
+              type={'primary'}
+              onClick={this.addQuestionHandler}
+            >
+              Добавить вопрос
+            </Button>
+
+            <Button
+              type={'success'}
+              onClick={this.createQuizHandler}
+            >
+              Создать тест
+            </Button>
+
+          </form>
+        </div>
       </div>
     )
   }
