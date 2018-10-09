7d9f90aa522ecd38980d6e5b7e5628ce98c39c10
Pavel Ravvich <triodjangopiter@yandex.ru>
2018-10-09 19:49:08 +0300

    Add backend request for pull quiz list

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
 import classes from './QuizList.css';
 import {NavLink} from "react-router-dom";
+import axios from 'axios'
 
 class QuizList extends Component {
 
+  state = {
+    quizes: []
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
         <li
-          key={index}
+          key={quiz.id}
         >
-          <NavLink to={'/quiz/' + quiz}>
-            Тест {quiz}
+          <NavLink to={'/quiz/' + quiz.id}>
+            {quiz.name}
           </NavLink>
         </li>
       )
     })
+  }
+
+  async componentDidMount() {
+    try {
+      const resp = await axios.get('https://react-quiz-17f6c.firebaseio.com/quizes.json');
+      const quizes = [];
+      Object.keys(resp.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`
+        })
+      });
+      this.setState({
+        quizes
+      });
+      console.log()
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
